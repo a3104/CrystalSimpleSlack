c15dd54c72da41d0337d4dfdb62dd0398486d318
abe <a3104@accelia.net>
2018-07-02 17:07:59 +0900

    fix test

--- spec/simpleSlack_spec.cr
+++ spec/simpleSlack_spec.cr
@@ -5,6 +5,8 @@ include SimpleSlack
 describe SimpleSlack do
   # TODO: Write tests
 
+  slack = SimpleSlack::Notifier.new("")
+
   it "test of create_jeson" do
     slack.create_message_json("#test", "tester", "test", ":ghost").should eq %{{"channel":"#test","username":"tester","text":"test","icon_emoji":":ghost"}}
     slack.create_message_json("#test", "tester", "test", ":ghost", NotifyLevel::GOOD).should eq %{{"channel":"#test","username":"tester","text":"test","icon_emoji":":ghost","color":"good"}}
