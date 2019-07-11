2664c0a9258fafe7e7bdcbe88160694f545a8d8c
abe <a3104@accelia.net>
2019-07-11 20:09:09 +0900

    implement Private Channel List

--- src/models/channel_list.cr
+++ src/models/channel_list.cr
@@ -1,6 +1,25 @@
 require "http/client"
 
 module SimpleSlack
+  class PrivateChannelList
+    @token : String
+    @base_url = "https://slack.com/api/groups.list"
+    @body : JSON::Any
+    @channel_id_hash : Hash(String, String)
+
+    def initialize(token : String)
+      @token = token
+      response = HTTP::Client.get("#{@base_url}?token=#{@token}")
+      raise "failed statusCode #{response.status_code} slack list api" unless response.status_code.to_s == "200"
+      @body = JSON.parse(response.body)
+      @channel_id_hash = @body["channels"].as_a.map { |channel| [channel["name"].as_s, channel["id"].as_s] }.to_h
+    end
+
+    def get_id_by_name?(channel_name)
+      @channel_id_hash[channel_name]?
+    end
+  end
+
   class ChannelList
     @token : String
     @base_url = "https://slack.com/api/channels.list"
