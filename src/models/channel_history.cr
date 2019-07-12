9fb4da0a3c656067fc110bc9abd1784075368fa4
abe <a3104@accelia.net>
2019-07-12 16:08:02 +0900

    implement PrivateChannelHistory(Group History)

--- src/models/channel_history.cr
+++ src/models/channel_history.cr
@@ -1,6 +1,23 @@
 require "http/client"
 
 module SimpleSlack
+
+  class PrivateChannelHistory
+    @token : String
+    @base_url = "https://slack.com/api/groups.history"
+
+    def initialize(token : String)
+      @token = token
+    end
+
+    def get_message(channel_id : String, count = 1000, from = 0, to = Time.new.to_unix_ms / 1000)
+      url = "#{@base_url}?token=#{@token}&channel=#{channel_id}&count=#{count}&oldest=#{from}&latest=#{to}&pretty=1"
+      response = HTTP::Client.get(url)
+      raise "failed statusCode #{response.status_code} slack PrivateChannelHistory api" unless response.status_code == 200
+      Json.parse(response.body)
+    end
+  end
+
   class ChannelHistory
     @token : String
     @base_url = "https://slack.com/api/channels.history"
